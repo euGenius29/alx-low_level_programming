e948d68192f2b637640692b260323fdff19add3b
euGenius29 <angloweugenious29@gmail.com>
2023-03-22 19:25:58 -0700

    testing the waters

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - Print the string _putchar.
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This 0x02 project folder has solutions to source files that 
+helps us understand functions,nested functions and others.
