f4844bfdfdfa024dda62ea0ae6ba24a45a589785
euGenius29 <angloweugenious29@gmail.com>
2023-09-20 04:03:11 -0700

    Submitting 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,8 +1,8 @@
-include "main.h"
-include "string.h"
+#include "main.h"
+#include <string.h>
 /**
  * print_rev - prints the string in reverse
- *
+ * @s: the string to be reversed
  * Return: 0
  */
 void print_rev(char *s)
@@ -12,6 +12,6 @@ void print_rev(char *s)
 	for (i = strlen(s) - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
-
+	}
 	_putchar('\n');
 }
