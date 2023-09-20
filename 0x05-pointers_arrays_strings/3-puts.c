18ad7b6bcd2c8f2c8d9bb19868f017a093202bc4
euGenius29 <angloweugenious29@gmail.com>
2023-09-20 03:25:31 -0700

    Uploading 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * _puts - prints the characters
- *
+ * _puts - prints the characters followed by a new line to stdout
+ * @str: input for prototype
  * return: void
  */
 void _puts(char *str)
 {
 	int i;
 
-	for (i = 0; i != '\0'; i++)
+	i = 0;
+	while (*str != '\0')
 	{
-		_putchar(str[i]);
+		_putchar(*str);
+		str++;
+		i++;
 	}
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,6 +1,10 @@
 #include <unistd.h>
 /**
- * _putchar - puts char
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
