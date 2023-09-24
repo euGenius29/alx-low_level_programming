a48409c6096b504c13d1430e23edfd804cf28320
euGenius29 <angloweugenious29@gmail.com>
2023-09-23 21:23:02 -0700

    Submitting the first 3

---
+++ 0x06-pointer_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings using n bytes at most from src
+ * @dest: first input
+ * @src: adjoining input
+ * @n: specifies the number of characters to concatenate
+ *
+ * Return: dest
+ */
+char *_strncat (char *dest, char *src, int n)
+{
+	int x, y;
+
+	x = 0;
+	while ( *dest)
+		x++;
+	y = 0;
+	while (y < n && src[y] != '\0')
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+	dest[x] = '\0';
+	return(dest);
