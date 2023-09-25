1f0c5c2565b4a3028cf83cb412add962fdc75255
euGenius29 <angloweugenious29@gmail.com>
2023-09-24 18:08:54 -0700

    Back to finish this

---
+++ 0x06-pointer_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ * _strcmp - compares string values
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: the integer value from the function
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
---
+++ 0x06-pointer_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * reverse_array - reverse array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: void
+ */
+
+int reverse_array(int *a, int n)
+{
+	int i, j;
+	
+	j = 0;
+	for (i = 0; i < n/2; i++)
+	{
+		j = a[i];
+		a[i] = a[n-i-1];
+		a[n-i-1] = j;
+	}
+}
