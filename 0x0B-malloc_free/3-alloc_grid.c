3894dd05e4f8a3ba09a4d643d52804bd7f4c19f8
euGenius29 <angloweugenious29@gmail.com>
2023-10-03 19:26:40 -0700

    alloc grid and free grid upload

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,9 +4,34 @@
  * alloc_grid - creates a 2-D array and initializes it with a specific char
  * @width: array width
  * @height: array height
- * return: pointer to memory on success, NULL on failure.
+ * Return: pointer to memory on success, NULL on failure.
  */
 int **alloc_grid(int width, int height);
-(
- int i,j;
- int **soln
+{
+	int i, j;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
+}
