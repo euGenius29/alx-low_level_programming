87d20b41f97c4c06072f9f7d62d380a5abfd87c5
euGenius29 <angloweugenious29@gmail.com>
2023-10-05 20:29:12 -0700

    first submissions

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - returns pointer to mem loc
+ * @b: size of memory to allocate
+ * Return: pointer to loc on success or exit(98) on failure
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+	p = malloc(b);
+	if (p == NULL)
+		exit (98);
+	return (p);
+}
