f2c838cc4084cab0067ab599bdf08fa60404e2ec
euGenius29 <angloweugenious29@gmail.com>
2023-10-05 22:51:07 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements
+ * @size: size of bytes to allocate
+ * Return: ptr to allocated memory or NULL on failure
+ */
+void *_calloc(unsigned int nmemb, unsigned int size);
+{
+	unsigned int i, j, count;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	count = nmemb * size;
+	ptr = malloc(count);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < count; i++)
+	{
+		*(ptr + i) = 0;
+	}
+	return (ptr);
+}
+
