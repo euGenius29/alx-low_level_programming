4c560251d65e7680be30b8ee6aac1d2659dc2e95
euGenius29 <angloweugenious29@gmail.com>
2023-10-12 22:30:36 -0700

    dusting up for function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds ints
+ * @a: first int
+ * @b: second int
+ * @s: operator
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts ints
+ * @a: first int
+ * @b: second int
+ * @s: operator
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+/**
+ * op_mul - multiplies ints
+ * @a: first int
+ * @b: second int
+ * @s: operator
+ * Return: multiple
+ */
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+ * op_div - divides ints
+ * @a: first int
+ * @b: second int
+ * @s: operator
+ * Return: dividend
+ */
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+/**
+ * op_add - finds remainder
+ * @a: first int
+ * @b: second int
+ * @s: operator
+ * Return: remainder
+ */
+int op_add(int a, int b)
+{
+        return (a % b);
+}
--- 0x0F-function_pointers/calc.h
+++ 0x0F-function_pointers/calc.h
@@ -1,14 +1,8 @@
-/**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
- */
-typedef struct op
-{
-    char *op;
-    int (*f)(int a, int b);
-} op_t;
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
