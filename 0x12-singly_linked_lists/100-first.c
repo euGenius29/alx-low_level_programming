70d832fe92d252efd88a9164ae98f4e0660801e1
euGenius29 <angloweugenious29@gmail.com>
2023-10-20 09:29:53 -0700

    betty: documentation and style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,9 +5,11 @@
  * Return: void
  */
 
-void before_main() __attribute__((constructor));
+void before_main(void) __attribute__((constructor));
 
-void before_main()
+
+void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
