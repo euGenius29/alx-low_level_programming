845f2816f7737660cfc4e1c9ebd7ebc09899403e
euGenius29 <angloweugenious29@gmail.com>
2023-10-20 09:07:05 -0700

    Adding first advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * before_main - prints a formatted string before the main function
+ * Return: void
+ */
+
+void before_main() __attribute__((constructor));
+
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
