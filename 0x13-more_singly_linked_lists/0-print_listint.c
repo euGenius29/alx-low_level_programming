98c27b25d2fae8b03e39eb2faadee110a89acd4b
euGenius29 <angloweugenious29@gmail.com>
2023-10-23 23:03:59 -0700

    Betty corrections

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,7 +11,7 @@
 size_t print_listint(const listint_t *t)
 {
 	size_t count;
-	
+
 	if (!t)
 		return (0);
 	count = 0;
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (!NewNode)
 	{
 		printf("Memory allocation failed\n");
-		return(NULL);
+		return (NULL);
 	}
 	NewNode->n = n;
 	NewNode->next = *head;
