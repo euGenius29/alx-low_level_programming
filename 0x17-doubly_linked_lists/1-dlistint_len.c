c34c5ddb9462324f378468c9364ec7b469930faf
euGenius29 <angloweugenious29@gmail.com>
2023-12-09 10:53:01 -0800

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all elements of a dlistint_t list
+ * @h: the head pointer
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+
+	size_t i = 0;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		i++;
+	}
+	return (i);
+}
+
